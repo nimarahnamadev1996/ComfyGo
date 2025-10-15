597ae008dd27ea1d3e6033c32a811cd6b0df1144
nimarahnamadev1996 <nimarahnamadev1996@gmail.com>
2025-10-15 09:54:08 +0330

    work on Reservations page and action - Rentals page and action

--- my-app/src/components/form/AmenitiesInput.tsx
+++ my-app/src/components/form/AmenitiesInput.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useState } from 'react';
 
 
@@ -8,14 +7,19 @@ import { Checkbox } from '@/components/ui/checkbox';
 
 
 
-const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
+function AmenitiesInput({ defaultValue }: { defaultValue?: Amenity[] }) {
 
+  const amenitiesWithIcons = defaultValue?.map(({ name, selected }) => ({
+    name,
+    selected,
+    icon: amenities.find((amenity) => amenity.name === name)!.icon,
+  }));
+  
   const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(
-    defaultValue || amenities
+    amenitiesWithIcons || amenities
   );
 
-
-    const handleChange = (amenity: Amenity) => {
+  const handleChange = (amenity: Amenity) => {
     setSelectedAmenities((prev) => {
       return prev.map((a) => {
         if (a.name === amenity.name) {
@@ -28,31 +32,31 @@ const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
 
   return (
     <section>
+
       <input
         type='hidden'
         name='amenities'
-        value={JSON.stringify(selectedAmenities)}
-      />
+        value={JSON.stringify(selectedAmenities)}/>
+
       <div className='grid grid-cols-2 gap-4'>
-        {selectedAmenities.map((amenity) => (
-          <div key={amenity.name} className='flex items-center space-x-2'>
-            <Checkbox
-              id={amenity.name}
-              checked={amenity.selected}
-              onCheckedChange={() => handleChange(amenity)}
-            />
-            <label
-              htmlFor={amenity.name}
-              className='text-sm font-medium leading-none capitalize flex gap-x-2 items-center'
-            >
-              {amenity.name}
-              <amenity.icon className='w-4 h-4' />
-            </label>
-          </div>
-        ))}
+        {selectedAmenities.map((amenity) => {
+          return (
+            <div key={amenity.name} className='flex items-center space-x-2'>
+              <Checkbox
+                id={amenity.name}
+                checked={amenity.selected}
+                onCheckedChange={() => handleChange(amenity)}/>
+              <label
+                htmlFor={amenity.name}
+                className='text-sm font-medium leading-none capitalize flex gap-x-2 items-center'
+>
+                {amenity.name} <amenity.icon className='w-4 h-4' />
+              </label>
+            </div>
+          );
+        })}
       </div>
     </section>
-  )
+  );
 }
-
-export default AmenitiesInput
+export default AmenitiesInput;
